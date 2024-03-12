86bc4195bde14011d28a3bb8c192a186060013f2
Ullas <118278305+Ullasgs@users.noreply.github.com>
2024-03-12 18:32:46 +0530

    q

--- OOPS lab/a.cpp
+++ OOPS lab/a.cpp
@@ -1,32 +1,15 @@
 #include <iostream>
 using namespace std;
-class ex
-{
-    int **ptr;
-
-public:
-    ex()
-    {
-        ptr = new int *[5];
-        for (int i = 0; i < 5; i++)
-            ptr[i] = new int[5];
-        for (int i = 0; i < 5; i++)
-            for (int j = 0; j < 5; j++)
-                ptr[i][j] = 5; // initializing
-    }
-    void display()
-    {
-        cout << "Value at newly allocated memory is\n";
-        for (int i = 0; i < 5; i++)
-        {
-            for (int j = 0; j < 5; j++)
-                cout << ptr[i][j] << "\t";
-            cout << "\n";
-        }
-    }
-};
-int main()
-{
-    ex e1;
-    e1.display();
+const int MAX = 4;
+int main () {
+const char *names[MAX] = { "Bangalore", "Hubli", "Tumkur", "Mysore" };
+for (int i = 0; i < MAX; i++) {
+cout << "Address of each names[" << i << "] = ";
+cout << (names + i) << endl;
+}
+for (int i = 0; i < MAX; i++) {
+cout << "Value of names[" << i << "] = ";
+cout << *(names + i) << endl;
+}
+return 0;
 }
